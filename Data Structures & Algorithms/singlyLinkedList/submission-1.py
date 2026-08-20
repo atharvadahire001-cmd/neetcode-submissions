5585f1b598d006c4ccfa5e53a144ded50eca466e
atharvadahire001-cmd <atharvadahire001@gmail.com>
2026-08-20 22:27:53 +0530

    Add: singlyLinkedList - submission-1

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-1.py
@@ -0,0 +1,60 @@
+class ListNode():
+    def __init__(self,val,next_node=None):
+        self.val=val
+        self.next = next_node
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head=ListNode(-1)
+        self.tail=self.head
+
+    
+    def get(self, index: int) -> int:
+        curr=self.head.next
+        i=0
+        while curr:
+            if i== index:
+                return curr.val
+            
+            i+=1
+            curr=curr.next
+        return -1
+
+        
+
+    def insertHead(self, val: int) -> None:
+        next_node= ListNode(val)
+        next_node.next=self.head.next
+        self.head.next = next_node
+        if not next_node.next:
+            self.tail= next_node
+        
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next=ListNode(val)
+        self.tail=self.tail.next
+        
+
+    def remove(self, index: int) -> bool:
+        curr = self.head
+        i=0
+        while i<index and curr:
+            i+=1
+            curr= curr.next
+
+        if curr and curr.next:
+            if curr.next== self.tail:
+                self.tail=curr
+            curr.next=curr.next.next
+            return True 
+        return False        
+        
+
+    def getValues(self) -> List[int]:
+        curr=self.head.next
+        list=[]
+        while curr:
+            list.append(curr.val)
+            curr=curr.next
+        return list
